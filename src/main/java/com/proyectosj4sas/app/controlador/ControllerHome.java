71cf78306124aae98eeec329206b988c8d92efe9
Najiluc <juli.navarro.13@gmail.com>
2022-06-21 16:59:51 -0500

    slash problem

--- src/main/java/com/proyectosj4sas/app/controlador/ControllerHome.java
+++ src/main/java/com/proyectosj4sas/app/controlador/ControllerHome.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ControllerHome {
 
-	@GetMapping("/")
+	@GetMapping("")
 	public String index(Model model) {
 		model.addAttribute("titulo", "PROYECTOS E INGENIERIA J4 S.A.");
 		model.addAttribute("ruta_de_navegacion", "Vista Principal");
 		return "index";
 	}
 	
-	@GetMapping("/registrar")
+	@GetMapping("registrar")
 	public String registro(Model model) {
 		model.addAttribute("titulo", "PROYECTOS E INGENIERIA J4 S.A.");
 		model.addAttribute("ruta_de_navegacion", "Registrar");
-		return "/vistas/registro/index";
+		return "vistas/registro/index";
 	}
-	@GetMapping("/test")
+	@GetMapping("test")
 	public String index2(Model model) {
 		return "pruebas";
 	}
